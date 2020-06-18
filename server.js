0cea71eaef6574687dc5cd5e7f062f1c05964289
Lanza <lanzathm@gmail.com>
2020-06-18 19:40:17 -0300

    Server config

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const nunjucks = require('nunjucks')
-const routes = require('./routes')
+const express = require('express');
+const nunjucks = require('nunjucks');
+const routes = require('./routes');
+const methodOverride = require('method-override');
 
-const server = express()
+const server = express();
 
 /**
  * Middlewares
  */
-server.use(express.urlencoded({ extended: true })) // middleware for req.body
-server.use(express.static('public'))
-server.use(routes)
+server.use(express.urlencoded({ extended: true })); // middleware for req.body
+server.use(express.static('public'));
+server.use(methodOverride('_method'));
+server.use(routes);
 
-server.set('view engine', 'njk')
+server.set('view engine', 'njk');
 
 /**
  * Nunjucks file config
@@ -20,9 +22,9 @@ nunjucks.configure('views', {
     express: server,
     autoescape: false,
     noCache: true
-})
+});
 
 /**
  * Server on port 5000
  */
-server.listen(5000)
+server.listen(5000);

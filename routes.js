20ca8452559079687e75fa6089da1f64ca2f86fc
Lanza <lanzathm@gmail.com>
2020-07-20 19:37:36 -0300

    :sparkles: Members feature created

--- routes.js
+++ routes.js
@@ -4,29 +4,33 @@
  */
 const express = require('express');
 const routes = express.Router();
-const instructors = require('./src/instructors');
+const instructors = require('./controllers/instructors');
+const members = require('./controllers/members');
 
-
+/*==== Instructors endpoints ====*/
 routes.get('/', (req, res) => {
     return res.redirect('/instructors')
 });
 
 routes.get('/instructors', instructors.list);
-
 routes.get('/instructors/create', (req, res) => {
   return res.render('instructors/create')
 });
-
 routes.get('/instructors/:id', instructors.show);
 routes.get('/instructors/:id/edit', instructors.edit);
-
 routes.post('/instructors', instructors.post);
 routes.put('/instructors', instructors.put);
-
 routes.delete('/instructors', instructors.delete);
 
-routes.get('/members', (req, res) => {
-    return res.render('members')
+/*==== Members endpoints ====*/
+routes.get('/members', members.list);
+routes.get('/members/create', (req, res) => {
+  return res.render('members/create')
 });
+routes.get('/members/:id', members.show);
+routes.get('/members/:id/edit', members.edit);
+routes.post('/members', members.post);
+routes.put('/members', members.put);
+routes.delete('/members', members.delete);
 
 module.exports = routes;

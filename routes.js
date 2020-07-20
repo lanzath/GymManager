d6e5e614a55c27cecc14027187b221386a2a017f
Lanza <lanzathm@gmail.com>
2020-07-20 20:15:07 -0300

    :building_construction: Routes updated

--- routes.js
+++ routes.js
@@ -7,15 +7,13 @@ const routes = express.Router();
 const instructors = require('./controllers/instructors');
 const members = require('./controllers/members');
 
-/*==== Instructors endpoints ====*/
 routes.get('/', (req, res) => {
     return res.redirect('/instructors')
 });
 
+/*==== Instructors endpoints ====*/
 routes.get('/instructors', instructors.list);
-routes.get('/instructors/create', (req, res) => {
-  return res.render('instructors/create')
-});
+routes.get('/instructors/create', instructors.create);
 routes.get('/instructors/:id', instructors.show);
 routes.get('/instructors/:id/edit', instructors.edit);
 routes.post('/instructors', instructors.post);
@@ -24,9 +22,7 @@ routes.delete('/instructors', instructors.delete);
 
 /*==== Members endpoints ====*/
 routes.get('/members', members.list);
-routes.get('/members/create', (req, res) => {
-  return res.render('members/create')
-});
+routes.get('/members/create', members.create);
 routes.get('/members/:id', members.show);
 routes.get('/members/:id/edit', members.edit);
 routes.post('/members', members.post);

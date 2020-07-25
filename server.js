2a68e1faf9c4c2f2035f01fbf0447df1e592cdfb
Lanza <lanzathm@gmail.com>
2020-07-24 23:15:25 -0300

    :pencil: Few ajdusts on comments

--- server.js
+++ server.js
@@ -1,30 +1,26 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const routes = require('./routes');
+// Using method override for PUT and DELETE HTTP verbs
 const methodOverride = require('method-override');
 
 const server = express();
 
-/**
- * Middlewares
- */
-server.use(express.urlencoded({ extended: true })); // middleware for req.body
+/* Middlewares */
+// middleware for req.body
+server.use(express.urlencoded({ extended: true }));
 server.use(express.static('public'));
 server.use(methodOverride('_method'));
 server.use(routes);
 
 server.set('view engine', 'njk');
 
-/**
- * Nunjucks file config
- */
+/* Nunjucks file config */
 nunjucks.configure('views', {
     express: server,
     autoescape: false,
     noCache: true
 });
 
-/**
- * Server on port 5000
- */
+/* Server running on port 5000 */
 server.listen(5000);

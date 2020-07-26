cc1356a24cdc056be12b2b1f263a82f62506b9ab
Lanza <lanzathm@gmail.com>
2020-07-25 23:38:02 -0300

    :bulb: Commented js script

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,8 @@
+// Get the current page path
 const currentPage = location.pathname;
 const menuItems = document.querySelectorAll('header .links a');
 
+// Iterates menu links and applies class active into each element
 for (item of menuItems) {
     if (currentPage.includes(item.getAttribute('href'))) {
         item.classList.add('active');

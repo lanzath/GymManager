e9b31649df7e44ef65cb2a3a4d59456569a3bf32
Lanza <lanzathm@gmail.com>
2020-08-16 14:50:54 -0300

    :sparkles: Post method done

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -1,4 +1,5 @@
 const { age, date } = require('../lib/utils');
+const db = require('../config/db');
 
 module.exports = {
   /**
@@ -8,8 +9,11 @@ module.exports = {
    * @returns Render index view
    */
   index(req, res) {
-    // Render index view and send object instructors with data.instructors value
-    return res.render('instructors/index');
+    db.query(`SELECT * FROM instructors`, (err, results) => {
+      if (err) return res.send({Error: 'Erro ao conectar ao banco de dados :('})
+
+      return res.render('instructors/index', {instructors: results.rows});
+    });
   },
 
   /**
@@ -39,9 +43,33 @@ module.exports = {
       }
     });
 
-    let { avatar_url, birth, name, services, gender } = req.body;
+    // $1, $2, ..., $6 are placeholders for being switched with values
+    const query = `
+      INSERT INTO instructors (
+        name,
+        avatar_url,
+        gender,
+        services,
+        birth,
+        created_at
+      ) VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING id
+    `
 
-    return
+    const values = [
+      req.body.name,
+      req.body.avatar_url,
+      req.body.gender,
+      req.body.services,
+      date(req.body.birth).iso,
+      date(Date.now()).iso, //created_at
+    ];
+
+    db.query(query, values, (err, results) => {
+      if (err) return res.send({Error: 'Erro ao conectar ao banco de dados :('})
+
+      return res.redirect(`/instructors/${results.rows[0].id}`)
+    });
   },
 
   /**

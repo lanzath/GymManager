bfd44e2259330a3c025e463d83b869fcd602099f
Lanza <lanzathm@gmail.com>
2020-08-23 23:13:31 -0300

    :sparkles: Paginate logic script done

--- public/scripts.js
+++ public/scripts.js
@@ -8,3 +8,34 @@ for (item of menuItems) {
     item.classList.add('active');
   }
 };
+
+// Paginate
+// [1, ..., 13, 14, 15, 16, 17, ..., 20]
+let totalPages = 20,
+    selectedPage = 15,
+    pages = [],
+    oldPage;
+
+// push to pages array numbers as example array (line 13)
+for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+  const firstAndLastPage = currentPage == 1 || currentPage == totalPages;
+  const pagesAfterSelectedPage = currentPage <= selectedPage + 2;
+  const pagesBeforeSelectedPage = currentPage >= selectedPage - 2;
+
+  if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+
+    if (oldPage && currentPage - oldPage > 2) {
+      pages.push('...');
+    }
+
+    if (oldPage && currentPage - oldPage == 2) {
+      pages.push(oldPage + 1);
+    }
+
+    pages.push(currentPage);
+
+    oldPage = currentPage;
+  }
+}
+
+console.log(pages);
